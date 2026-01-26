6262e23e600ecedc7884015a90829034912a01e2
MihaSuperCod <mihaelamoga23@gmail.com>
2026-01-26 11:38:11 +0200

    Acess Control concept

---
+++ src/test/java/accessControlV1/Fruct.java
@@ -0,0 +1,15 @@
+package accessControlV1;
+
+public class Fruct {
+
+    //public, private, protected, default(nu inseamna public)
+    //doua categorii:
+    //1. cand vine vorba de mostenire (in acelasi pachet/in pachete diferite)
+    //2. cand vine vorba de obiect (in acelasi pachet/ in pachete diferite)
+
+    public void metodaPublica(){}
+    private void metodaPrivata(){}
+    protected void metodaProtected(){}
+    void metodaDefault(){}
+
+}
