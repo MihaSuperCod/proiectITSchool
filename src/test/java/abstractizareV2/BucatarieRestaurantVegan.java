0341a6bb591600a8b02847d548e61005b049083d
MihaSuperCod <mihaelamoga23@gmail.com>
2026-01-26 17:19:13 +0200

    Abnstractizare Clasa Abstracta

---
+++ src/test/java/abstractizareV2/BucatarieRestaurantVegan.java
@@ -0,0 +1,42 @@
+package abstractizareV2;
+
+import abstractizareV1.BucatarieRestaurant;
+
+public class BucatarieRestaurantVegan extends Restaurant {
+
+    @Override
+    public void servesteHummus() {
+
+    }
+
+    @Override
+    public void servesteSalataVerde() {
+
+    }
+
+    @Override
+    public void servesteSucAvocado() {
+
+    }
+
+    @Override
+    public void servesteBurger() {
+
+    }
+
+    @Override
+    public void servestePaste() {
+
+    }
+
+    @Override
+    public void servesteCostite() {
+
+    }
+
+    @Override
+    public void servestePizza() {
+
+    }
+}
+
